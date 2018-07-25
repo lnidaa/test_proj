b4623abe87d0c656f91c546d2231bd37487e785b
lnidaa <daria.narizhna@gotoinc.eu>
2018-07-25 12:09:02 +0300

    Added aws credentials to .env file

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
       # Настройка для Amazon S3
       :provider => 'AWS',
-      :aws_access_key_id => 'AKIAIBI7KS22IIUIW7EQ',
-      :aws_secret_access_key => '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',
-      :region => 'us-east-2'
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => ENV['REGION_AWS']
   }
   config.fog_directory = 'mytestikbucket'
 end
